02875ae44ad066da049034fe5bdcc17e54eb717e
Manti Mokone <mokonemanti256@gmail.com>
2025-01-14 02:07:27 -0500

    Models registered in the admin.py

--- server/backend/api/admin.py
+++ server/backend/api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from api.models import User,TextSubmission,Analysis,AiModelData
 
 # Register your models here.
+admin.site.register(User,TextSubmission,Analysis,AiModelData)
