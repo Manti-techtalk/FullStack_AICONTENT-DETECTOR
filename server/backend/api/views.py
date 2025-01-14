d5ebef0bba4990396f934661220f5d9ed5da8d3a
Manti Mokone <mokonemanti256@gmail.com>
2025-01-14 02:22:35 -0500

    Resting

--- server/backend/api/views.py
+++ server/backend/api/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from api.models import User,Analysis,AiModelData,TextSubmission
+from api.serializers import UserSerializer,AnalysisSerializer,AiModelDataSerializer,TextSubmissionSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
+
+
+
+
+
+
+
 def testing(request):
     return HttpResponse('<h1>Testing API</h1>')
