0dc8fad1244f049ed792bbb2360fd41c0854d602
Manti Mokone <mokonemanti256@gmail.com>
2025-01-15 17:21:47 -0500

    Urls patter defined

--- server/backend/api/urls.py
+++ server/backend/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path,include
 from . import views
+from api.views import PstGet
 
 urlpatterns = [
-    path('',views.testing)
+    path('',views.testing),
+    path('alldata/', PstGet)
 ]
